ab835594071d4a6a32406850a4d64540fce72bff
uditsharma1 <suditsharma51@gmail.com>
2021-07-06 15:55:03 +0530

    my project

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package in.co.corejava;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=5;
+		System.out.println(a+b);
+
+	}
+
+}
